13d17fddf011d438fbcbb239f25e7cb9a3910584
alynwhite <alexia.white135@gmail.com>
2024-11-09 23:10:11 -0700

    finished prep 3 '

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 12);
+        int kount = 0;
+
+        do{
+            Console.WriteLine("What is the magic number? ");
+            string mNum = Console.ReadLine();
+            int magic_num = int.Parse(mNum);
+
+            if (magic_num == number)
+            {
+                 response = "You guessed it!";
+            }
+            else if (magic_num < number){
+                response = "Higher";
+            }
+            else{
+                response ="Lower";
+            }
+            Console.WriteLine($"{response}");
+            kount ++;
+        }while (response != "You guessed it!");
+        
     }
 }
