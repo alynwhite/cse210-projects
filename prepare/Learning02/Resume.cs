0a4f65aee08f4d93745ab6b453562aabe8bc8906
alynwhite <alexia.white135@gmail.com>
2024-11-13 21:38:46 -0700

    completed learning activity

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"Name: {_name}");
+        foreach (Job j in _jobs){
+            j.Display();
+        }
+    }
+}
