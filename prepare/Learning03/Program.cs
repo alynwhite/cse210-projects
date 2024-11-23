5a5a92542773568c5cb5181383308b6c32645db3
alynwhite <alexia.white135@gmail.com>
2024-11-22 18:43:24 -0700

    learning activity w3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 2);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetFractionString());
+
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        f1.SetTop(2);
+        f1.SetBottom(3);
+
+        Console.WriteLine(f1.GetFractionString());
     }
 }
